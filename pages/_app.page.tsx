8d5c93e8750e2115d0b2c8a114d52dfac6117515
tesso57 <tesso1729@gmail.com>
2022-09-28 16:27:03 +0900

    💄 font Noto-sansの設定

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,7 +2,7 @@ import { css, Global } from '@emotion/react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'components/theme'
 import 'modern-normalize/modern-normalize.css'
-
+import 'styles/globals.css'
 const resetATag = css`
   a {
     color: inherit;
