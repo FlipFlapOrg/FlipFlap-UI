e706befaaff97c77d08721672f6922bdd40e0ed5
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-09-28 12:56:18 +0900

    👕

--- api/users/[user_id]/bookmarks.ts
+++ api/users/[user_id]/bookmarks.ts
@@ -13,36 +13,30 @@ export const bookmarks = (client: Client) => async (user_id: string) => {
 
 export const addBookmarks =
   (client: Client) => async (user_id: string, manga_id: string) => {
-    const res = await fetch(
-      `${client.baseUrl}/users/${user_id}/bookmarks`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          manga_id,
-        }),
-      }
-    )
+    const res = await fetch(`${client.baseUrl}/users/${user_id}/bookmarks`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        manga_id,
+      }),
+    })
     const data = await res.json()
     return data
   }
 
 export const removeBookmarks =
   (client: Client) => async (user_id: string, manga_id: string) => {
-    const res = await fetch(
-      `${client.baseUrl}/users/${user_id}/bookmarks`,
-      {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          manga_id,
-        }),
-      }
-    )
+    const res = await fetch(`${client.baseUrl}/users/${user_id}/bookmarks`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        manga_id,
+      }),
+    })
     const data = await res.json()
     return data
   }
